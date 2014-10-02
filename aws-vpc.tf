7300883101f40a0ce6f935ec224bbcc3adf828cd
Rob Parrott <robparrott@gmail.com>
2014-10-01 21:03:52 -0400

    setup proper route tables and internet gateway

--- aws-vpc.tf
+++ aws-vpc.tf
@@ -12,6 +12,14 @@ resource "aws_internet_gateway" "logserver_gw" {
     vpc_id = "${aws_vpc.logserver_vpc.id}"
 }
 
+resource "aws_route_table" "logserver_route_table" {
+    vpc_id = "${aws_vpc.logserver_vpc.id}"
+    route {
+        cidr_block = "0.0.0.0/0"
+        gateway_id = "${aws_internet_gateway.logserver_gw.id}"
+    }
+}
+
 resource "aws_subnet" "logserver_subnet1" {
     vpc_id = "${aws_vpc.logserver_vpc.id}"
 	availability_zone = "${var.aws_az1}"  
@@ -22,4 +30,14 @@ resource "aws_subnet" "logserver_subnet2" {
     vpc_id = "${aws_vpc.logserver_vpc.id}"
 	availability_zone = "${var.aws_az2}"    
     cidr_block = "10.0.2.0/24"
+}
+
+resource "aws_route_table_association" "logserver_subnet1_rtassoc" {
+    subnet_id = "${aws_subnet.logserver_subnet1.id}"
+    route_table_id = "${aws_route_table.logserver_route_table.id}"
+}
+
+resource "aws_route_table_association" "logserver_subnet2_rtassoc" {
+    subnet_id = "${aws_subnet.logserver_subnet2.id}"
+    route_table_id = "${aws_route_table.logserver_route_table.id}"
 }
